44a43d3a05e738a2b9c2e9ce813b39cd14fe26eb
dommontalto <dominic.montalto@fusionautomation.com>
2019-07-24 12:29:09 +0100

    Requesting Permission to Access Photo Library

--- CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -8,9 +8,29 @@
 
 import Foundation
 import UIKit
+import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        populatePhotos()
+    }
+   
     
+    private func populatePhotos() {
+        
+        PHPhotoLibrary.requestAuthorization { status in
+            
+            if status == .authorized {
+                
+                // access the photos from photo library
+                
+            }
+            
+        }
+        
+    }
     
 }
--- CameraFilter/ViewController.swift
+++ CameraFilter/ViewController.swift
@@ -13,8 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.navigationController?.navigationBar.prefersLargeTitles = true
+         self.navigationController?.navigationBar.prefersLargeTitles = true
         
     }
 
