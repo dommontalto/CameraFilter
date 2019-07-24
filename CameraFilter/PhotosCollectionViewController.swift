2028029ededc164b5b1ba387d07ac831bf69e5ed
dommontalto <dominic.montalto@fusionautomation.com>
2019-07-24 12:31:06 +0100

    Displaying Photos from Photo Library

--- CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -20,6 +20,34 @@ class PhotosCollectionViewController: UICollectionViewController {
         populatePhotos()
     }
    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else {
+            fatalError("PhotoCollectionViewCell is not found")
+        }
+        
+        let asset = self.images[indexPath.row]
+        let manager = PHImageManager.default()
+        
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
+            
+            DispatchQueue.main.async {
+               cell.photoImageView.image = image
+            }
+            
+        }
+        
+        return cell
+        
+    }
     
     private func populatePhotos() {
         
@@ -35,8 +63,9 @@ class PhotosCollectionViewController: UICollectionViewController {
                 }
                 
                 self?.images.reverse()
-                print(self?.images)
-                //self?.collectionView.reloadData()
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
                 
             }
             
